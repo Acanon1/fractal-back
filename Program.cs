7f23d788a4a1b3322b305e66046a83ad23c2be24
Acanon24 <20180343@aloe.ulima.edu.pe>
2025-09-13 16:10:10 -0500

    final commit

--- Program.cs
+++ Program.cs
@@ -13,19 +13,28 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy => policy
+            .WithOrigins("http://localhost:5173") // <-- correct
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+
 
 var app = builder.Build();
 
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseHttpsRedirection();
 
 
+app.UseCors("AllowFrontend");
 
 
+app.UseAuthorization();
 app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.Run();
