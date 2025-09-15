22860b32455686fb0af2bf52d7423d884d5577ed
Acanon24 <20180343@aloe.ulima.edu.pe>
2025-09-15 12:25:49 -0500

    final commit for render

--- Program.cs
+++ Program.cs
@@ -1,17 +1,13 @@
-
 using Microsoft.EntityFrameworkCore;
 using fractal_back.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
-
-
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 
-
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+    options.UseNpgsql(connectionString));
+
 
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
@@ -21,29 +17,28 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend",
         policy => policy
-            .WithOrigins("http://localhost:5173")
+            .WithOrigins(
+                "http://localhost:5173",                     
+                "https://friendly-cajeta-54b347.netlify.app"       
+            )
             .AllowAnyHeader()
             .AllowAnyMethod());
 });
+
 
 builder.WebHost.ConfigureKestrel(options =>
 {
     options.ListenAnyIP(8080);
 });
 
-
 var app = builder.Build();
 
-app.UseHttpsRedirection();
 
-
-app.UseCors("AllowFrontend");
-
-
+app.UseCors("AllowFrontend");   
+app.UseHttpsRedirection();        
 app.UseAuthorization();
 app.MapControllers();
 app.UseSwagger();
 app.UseSwaggerUI();
-
 
 app.Run();
