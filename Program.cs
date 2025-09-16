778d56e8306c699790277c980c6e8f6bf4df9dff
Acanon24 <20180343@aloe.ulima.edu.pe>
2025-09-16 12:26:57 -0500

    fix connection string

--- Program.cs
+++ Program.cs
@@ -3,7 +3,8 @@ using fractal_back.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION")
+                       ?? builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(connectionString));
